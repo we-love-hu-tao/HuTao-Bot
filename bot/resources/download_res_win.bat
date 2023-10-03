5223ac68b892ce2d81b3ad14a920ad9a5acf38f7
Timur <timurbogdanov2008@gmail.com>
2023-10-03 22:52:10 +0300

    changed to resources from Dim - they're better

--- bot/resources/download_res_win.bat
+++ bot/resources/download_res_win.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set "TOKEN=YOUR-ACCESS-TOKEN"
-set "REPO=35984297"
+set "REPO=41287973"
 
 rem This script downloads these files:
 
@@ -13,17 +13,17 @@ rem WeaponExcelConfigData.json
 rem TextMapRU.json
 
 echo Downloading AvatarExcelConfigData.json
-curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/Resources%%2FExcelBinOutput%%2FAvatarExcelConfigData.json/raw" -o AvatarExcelConfigData.json
+curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/ExcelBinOutput%%2FAvatarExcelConfigData.json/raw" -o AvatarExcelConfigData.json
 echo Downloading AvatarSkillDepotExcelConfigData.json
-curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/Resources%%2FExcelBinOutput%%2FAvatarSkillDepotExcelConfigData.json/raw" -o AvatarSkillDepotExcelConfigData.json
+curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/ExcelBinOutput%%2FAvatarSkillDepotExcelConfigData.json/raw" -o AvatarSkillDepotExcelConfigData.json
 echo Downloading AvatarSkillExcelConfigData.json
-curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/Resources%%2FExcelBinOutput%%2FAvatarSkillExcelConfigData.json/raw" -o AvatarSkillExcelConfigData.json
+curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/ExcelBinOutput%%2FAvatarSkillExcelConfigData.json/raw" -o AvatarSkillExcelConfigData.json
 echo Downloading ManualTextMapConfigData.json
-curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/Resources%%2FExcelBinOutput%%2FManualTextMapConfigData.json/raw" -o ManualTextMapConfigData.json
+curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/ExcelBinOutput%%2FManualTextMapConfigData.json/raw" -o ManualTextMapConfigData.json
 echo Downloading WeaponExcelConfigData.json
-curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/Resources%%2FExcelBinOutput%%2FWeaponExcelConfigData.json/raw" -o WeaponExcelConfigData.json
+curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/ExcelBinOutput%%2FWeaponExcelConfigData.json/raw" -o WeaponExcelConfigData.json
 
 echo Downloading TextMapRU.json
-curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/Resources%%2FTextMap%%2FTextMapRU.json/raw" -o TextMapRU.json
+curl -sSfL -H "PRIVATE-TOKEN: %TOKEN%" "https://gitlab.com/api/v4/projects/%REPO%/repository/files/TextMap%%2FTextMapRU.json/raw" -o TextMapRU.json
 
 echo Done.
